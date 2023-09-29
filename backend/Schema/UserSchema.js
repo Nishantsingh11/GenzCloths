7d9585f1923e6aea10efea6282804977c791491c
Nishantsingh11 <ns114046@gmail.com>
2023-09-29 20:10:15 +0530

    first commit

---
+++ backend/Schema/UserSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // Additional user-related fields
+  firstName: String,
+  lastName: String,
+  dateOfBrith: Date,
+  gender: String,
+  phoneNumber: String,
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    postalCode: String,
+    country: String,
+  },
+  profileImage: String, // URL to the user's profile picture
+});
+export default mongoose.model("User", UserSchema);
